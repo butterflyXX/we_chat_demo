84f8ddc09baff94a09c50bd4559c6b4bec99f5ec
lxc <liuxiaochen1991@gmail.com>
2025-07-03 14:16:26 +0800

    feat: go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:chat_demo/common/navigator/history_observer.dart';
-import 'package:chat_demo/common/navigator/navigator_manager.dart';
 import 'package:chat_demo/route/route.dart';
 
 void main() async {
